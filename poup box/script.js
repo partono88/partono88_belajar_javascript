bdbc28f748daa8af6a49b15531f153158a15414d
partono88 <partono490@gmail.com>
2022-01-21 18:46:20 +0700

    menghapus komentar

--- poup box/script.js
+++ poup box/script.js
@@ -1,54 +1,54 @@
 // ###########alert di gunakan untuk menampilkan popup
-// alert("Nama!");
-// alert("Saya!");
-// alert("Partono!");
-// alert("Ok!");
+alert("Nama!");
+alert("Saya!");
+alert("Partono!");
+alert("Ok!");
 
 
 // ############prompt di gunakan untuk meminta inputan
-// var nama = prompt("masukan nama:");
-// alert(nama);
+var nama = prompt("masukan nama:");
+alert(nama);
 
-// ############confirm di gunakan untuk meminta konfirmasi dari user
-// var tes = confirm('kamu yakin??');
-// alert(tes)
+############confirm di gunakan untuk meminta konfirmasi dari user
+var tes = confirm('kamu yakin??');
+alert(tes)
 
 // ************contoh confirm pada luping while
-// alert("selamat datang");
-// var lagi = true;
-// while(lagi === true){
-// 	var nama = prompt('masukan nama:');
-// 	alert("halo" + nama)
-// 	lagi = confirm('coba lagi');
-// }
-// alert("trimakasih");
+alert("selamat datang");
+var lagi = true;
+while(lagi === true){
+	var nama = prompt('masukan nama:');
+	alert("halo" + nama)
+	lagi = confirm('coba lagi');
+}
+alert("trimakasih");
 
 // ########## peng kondisian if else dan else if
-// var angka = prompt('masukan angka');
-// if (angka % 2 == 0) {
-// 	alert(angka + 'adalah bilangan genap');
-// }else if(angka % 2 ==1){
-// 	alert(angka + 'adalah bilangan ganjil');
-// 	}else{
-// 		alert('yang ada masukan bukan angka');
-// 	}
+var angka = prompt('masukan angka');
+if (angka % 2 == 0) {
+	alert(angka + 'adalah bilangan genap');
+}else if(angka % 2 ==1){
+	alert(angka + 'adalah bilangan ganjil');
+	}else{
+		alert('yang ada masukan bukan angka');
+	}
 
 // ######## sintak swite
 
-// switch(ekspresi){
-// 	case "nilai 1":
-// 	aksi 1
-// 	[break;]
-// 	case "nilai 2":
-// 	aksi 2
-// 	[break;]
-// 	case "nilai n":
-// 	aksi n
-// 	[break;]
-// 	default:
-// 	aksi default
-// 	[break;]
-// }
+switch(ekspresi){
+	case "nilai 1":
+	aksi 1
+	[break;]
+	case "nilai 2":
+	aksi 2
+	[break;]
+	case "nilai n":
+	aksi n
+	[break;]
+	default:
+	aksi default
+	[break;]
+}
 
 
 var item = prompt('masukan nama makanan/minuman:\n (ontoh:nasi,daging,susu,hamburger,sofdrink)');
